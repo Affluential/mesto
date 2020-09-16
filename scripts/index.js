299b44b29f9cf3491d18b5b9055f0759385d7c9f
Sergey Egunov <affluential@yandex.com>
2020-09-16 17:43:01 +0300

    feature/addCard

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,24 @@
-const popup = document.querySelector(".popup");
+const popup = document.querySelector(".js__popup-input");
+const popupAdd = document.querySelector(".js__popup-add");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
-const addButton = document.querySelector(".profile__add-button");
+const closeAddButton = document.querySelector(".js__add_close");
 const saveForm = document.querySelector(".popup__container");
 const profileName = document.querySelector(".profile__name");
 const profileStatus = document.querySelector(".profile__status");
 const inputName = document.querySelector(".popup__input-item_change_name");
 const inputStatus = document.querySelector(".popup__input-item_change_status");
+const addButton = document.querySelector(".profile__add-button");
+const templateCard = document.querySelector(".template-card").content;
+const ulCards = document.querySelector(".cards");
+
+const addInputValue = document.querySelector(".popup__input-item_change_value");
+const addInputImage = document.querySelector(".popup__input-item_change_image");
+const addSaveForm = document.querySelector(".js__popup__container");
+
+const addSave = (e) => {
+  e.preventDefault();
+};
 
 const openPopup = () => {
   popup.classList.toggle("popup_is-opened");
@@ -16,6 +28,10 @@ const openPopup = () => {
 
 const closePopup = () => {
   popup.classList.toggle("popup_is-opened");
+};
+
+const toggleAddPopup = () => {
+  popupAdd.classList.toggle("popup_is-opened");
 };
 
 const popupCloseByClickOnShadow = () => {
@@ -64,10 +80,8 @@ const initialCards = [
       "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-console.log(initialCards);
+
 function addCard(cardObject, index) {
-  const templateCard = document.querySelector(".template-card").content;
-  const ulCards = document.querySelector(".cards");
   const htmlElement = templateCard.cloneNode(true);
   htmlElement.querySelector(
     ".card__image"
@@ -76,9 +90,33 @@ function addCard(cardObject, index) {
   ulCards.appendChild(htmlElement);
 }
 
+const submitSaveForm = (e) => {
+  e.preventDefault();
+  const newCard = {
+    name: addInputValue.value,
+    link: addInputImage.value,
+  };
+  const htmlElement = templateCard.cloneNode(true);
+  htmlElement.querySelector(
+    ".card__image"
+  ).style.backgroundImage = `url(${newCard.link})`;
+  htmlElement.querySelector(".card__title").innerText = newCard.name;
+  ulCards.prepend(htmlElement);
+  reset();
+  toggleAddPopup();
+};
+
+function reset() {
+  addInputValue.value = "";
+  addInputImage.value = "";
+}
+
 initialCards.forEach(addCard);
 
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
 saveForm.addEventListener("submit", saveInput);
 popup.addEventListener("click", popupCloseByClickOnShadow);
+addButton.addEventListener("click", toggleAddPopup);
+closeAddButton.addEventListener("click", toggleAddPopup);
+addSaveForm.addEventListener("submit", submitSaveForm);
